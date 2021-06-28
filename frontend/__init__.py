0e2cc4b6b1a0843f76f937049074626286a73544
rickwayne1125 <1037457338@qq.com>
2021-06-28 15:20:06 +0800

    ADD Enter Table Functions

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -7,7 +7,7 @@ from typing import Callable, Dict, List
 from backend import Table, Type
 
 
-def __choice_to_function(message: str, choices: Dict[str, Callable[[str], bool]], buffer: List[Table],
+def __choice_to_function(message: str, choices: Dict[str, Callable[[str], bool]], buffer: Dict[str, Table],
                          root_path: str) -> bool:
     return choices[
         PyInquirer.prompt([{
@@ -15,8 +15,17 @@ def __choice_to_function(message: str, choices: Dict[str, Callable[[str], bool]]
         }])['op']](buffer, root_path)
 
 
-def exit(buffer: List[Table], root_path) -> bool:
+def __choice_to_function2(message: str, choices: Dict[str, Callable[[str], bool]], table: Table,
+                          root_path: str) -> bool:
+    return choices[
+        PyInquirer.prompt([{
+            'type': 'list', 'name': 'op', 'message': message, 'choices': list(choices.keys())
+        }])['op']](table, root_path)
+
+
+def exit(buffer: Dict[str, Table], root_path) -> bool:
     print('Bye!')
+    print()
     return False
 
 
@@ -30,13 +39,29 @@ __create_table = [{
     'type': 'input', 'name': 'desc', 'message': 'Please Describe This Table:', 'validate': __not_null
 }]
 
+__enter_table = [{
+    'type': 'input', 'name': 'name', 'message': 'Please Enter The Table Name:', 'validate': __not_null
+}]
 
-def show_tables(buffer: List[Table], root_path) -> bool:
+__enter_field = [{
+    'type': 'input', 'name': 'name', 'message': 'Please Enter The Field Name:', 'validate': __not_null
+}, {
+    'type': 'input', 'name': 'desc', 'message': 'Please Describe This Field:', 'validate': __not_null
+    # }, {
+    #     'type': 'input', 'name': 'type', 'message': 'Please Describe This Table:', 'validate': __not_null
+}]
+
+__enter_desc = [{
+    'type': 'input', 'name': 'desc', 'message': 'Please Enter The Descption:', 'validate': __not_null
+}]
+
+
+def show_tables(buffer: Dict[str, Table], root_path) -> bool:
     headers = ['TableName', 'Description']
     data = []
     for table in buffer:
-        name = table.name
-        desc = table.desc
+        name = table
+        desc = buffer[table].desc
         row = [name, desc]
         data.append(row)
     print()
@@ -46,11 +71,61 @@ def show_tables(buffer: List[Table], root_path) -> bool:
     return True
 
 
-def enter_table(buffer: List[Table], root_path) -> bool:
+def show_table(buffer: Dict[str, Table], root_path) -> bool:
+    name = PyInquirer.prompt(__enter_table)['name']
+    # print(name)
+    __show_table(buffer, name)
+
     return True
 
 
-def create_table(buffer: List[Table], root_path) -> bool:
+def __show_table(buffer: Dict[str, Table], name: str):
+    headers = ['FieldName', 'Description']
+    data = []
+    table = buffer[name]
+    for field in table.fields:
+        row = [field.name, field.desc]
+        data.append(row)
+    print()
+    print(tabulate.tabulate(data, headers, tablefmt='pretty'))
+    print()
+
+
+def enter_table(buffer: Dict[str, Table], root_path) -> bool:
+    name = PyInquirer.prompt(__enter_table)['name']
+    __show_table(buffer, name)
+    table = buffer[name]
+    while __choice_to_function2('Select Your Option: ', {
+        'Add New Field': add_field,
+        'Remove Old Field': remove_field,
+        'Edit Description': edit_desc,
+        'Back': back
+    }, table, root_path):
+        pass
+    return True
+
+
+def back(table: Table, root_path):
+    return False
+
+
+def add_field(table: Table, root_path) -> bool:
+
+    return True
+
+
+def remove_field(table: Table, root_path) -> bool:
+    return True
+
+
+def edit_desc(table: Table, root_path) -> bool:
+    desc = PyInquirer.prompt(__enter_desc)['desc']
+    table.desc = desc
+    table.save(table)
+    return True
+
+
+def create_table(buffer: Dict[str, Table], root_path) -> bool:
     info = PyInquirer.prompt(__create_table)
     name = info['name']
     desc = info['desc']
@@ -58,9 +133,9 @@ def create_table(buffer: List[Table], root_path) -> bool:
     file = os.path.join(root_path, name + '.json')
     table = Table(file, name, desc, fields)
     table.save(table)
-    buffer.append(table)
+    buffer.setdefault(name, table)
     return True
 
 
-def drop_table(buffer: List[Table], root_path) -> bool:
+def drop_table(buffer: Dict[str, Table], root_path) -> bool:
     return True
--- main.py
+++ main.py
@@ -8,12 +8,13 @@ def main():
 
     print()
     print('Start Loading Metadata...')
-    buffer = []  # The Buffer for Tables
+    buffer = {}  # The Buffer for Tables
     for root, dirs, files in os.walk(root_path):
         for file in files:
             if os.path.splitext(file)[1] == '.json':
+                name = os.path.splitext(file)[0]
                 table = Table.load(os.path.join(root_path, file))
-                buffer.append(table)
+                buffer.setdefault(name, table)
     print('Metadata Successfully Loaded!')
     print(len(buffer), ' Tables Are Loaded in Total.')
     print()
@@ -21,6 +22,7 @@ def main():
     while fe.__choice_to_function('Select your operation: ',
                                   {
                                       'Show Tables': fe.show_tables,
+                                      'Show a Table': fe.show_table,
                                       'Enter a Table': fe.enter_table,
                                       'Create a Table': fe.create_table,
                                       'Drop a Table': fe.drop_table,
